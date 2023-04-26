2958401635a6f33bfa9022379b8068e623eb5103
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:10 +0300

    Update RouterModule.java

--- WebBasics/src/main/java/itstep/learning/ioc/RouterModule.java
+++ WebBasics/src/main/java/itstep/learning/ioc/RouterModule.java
@@ -4,6 +4,7 @@ import com.google.inject.servlet.ServletModule;
 import itstep.learning.filter.AuthFilter;
 import itstep.learning.filter.CharsetFilter;
 import itstep.learning.filter.DbCheckFilter;
+import itstep.learning.filter.FormsFilter;
 import itstep.learning.servlet.*;
 
 public class RouterModule extends ServletModule {
@@ -11,22 +12,24 @@ public class RouterModule extends ServletModule {
     protected void configureServlets() {
 
         // задаем фильтры
-        filter( "/*" ).through( CharsetFilter.class ) ;
-        // filter( "/*" ).through( DbCheckFilter.class ) ;
+        filter("/*").through(CharsetFilter.class);
+        filter("/*").through(FormsFilter.class);
+        // filter("/*").through(DbCheckFilter.class);
         // используя Regex отключаем фильтры для загрузки файлов (для ускорения работы)
-        filterRegex( "^/(?!image/.*).*$" ).through( DbCheckFilter.class ) ;
-        // filter( "/*" ).through( AuthFilter.class    ) ;
-        filterRegex( "^/(?!image/.*).*$" ).through( AuthFilter.class ) ;
+        filterRegex("^/(?!image/.*).*$").through(DbCheckFilter.class);
+        // filter("/*").through(AuthFilter.class);
+        filterRegex("^/(?!image/.*).*$").through(AuthFilter.class);
 
         // и сервлеты
-        serve( "/"          ).with( HomeServlet.class  ) ;
-        serve( "/home"      ).with( HomeServlet.class  ) ;
-        serve( "/forms"     ).with( FormsServlet.class ) ;
-        serve( "/about"     ).with( AboutServlet.class ) ;
-        serve( "/auth"      ).with( UserAuthServlet.class ) ;
-        serve( "/register"  ).with( UserRegisterServlet.class ) ;
-        serve( "/image/*"   ).with( DownloadServlet.class ) ;
-        serve( "/profile/*" ).with( UserProfileServlet.class ) ;
-        serve( "/story"     ).with( StoryServlet.class ) ;
+        serve("/").with(HomeServlet.class);
+        serve("/home").with(HomeServlet.class);
+        serve("/forms").with(FormsServlet.class);
+        serve("/about").with(AboutServlet.class);
+        serve("/auth").with(UserAuthServlet.class);
+        serve("/register").with(UserRegisterServlet.class);
+        serve("/image/*").with(DownloadServlet.class);
+        serve("/profile/*").with(UserProfileServlet.class);
+        serve("/story").with(StoryServlet.class);
+        serve("/team").with(TeamServlet.class);
     }
 }
