e07a39af13964306d79699064d0b1fabd4796ffd
pallyh <pallyx11@gmail.com>
2023-04-26 21:43:28 +0300

    Update UploadService.java

--- WebBasics/src/main/java/itstep/learning/service/UploadService.java
+++ WebBasics/src/main/java/itstep/learning/service/UploadService.java
@@ -1,46 +1,45 @@
 package itstep.learning.service;
 
-import java.io.File ;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class UploadService {
-    private final DiskFileItemFactory fileItemFactory ;
-    private final ServletFileUpload fileUpload ;
+    private final DiskFileItemFactory fileItemFactory;
+    private final ServletFileUpload fileUpload;
 
     public UploadService() {
-        fileItemFactory = new DiskFileItemFactory() ;
-        fileItemFactory.setSizeThreshold( 20480 ) ;  // размер файла в памяти. Если больше - на диск
-        File tmpDir = new File( "C:/tmp" ) ;
-        if( ! tmpDir.exists() ) {
-           if( ! tmpDir.mkdir() ) {
-                tmpDir = null ;
+        fileItemFactory = new DiskFileItemFactory();
+        fileItemFactory.setSizeThreshold(20480); // размер файла в памяти. Если больше - на диск
+        File tmpDir = new File("C:/tmp");
+        if (!tmpDir.exists()) {
+            if (!tmpDir.mkdir()) {
+                tmpDir = null;
             }
         }
-        if( tmpDir != null ) {
-            fileItemFactory.setRepository( tmpDir ) ;
+        if (tmpDir != null) {
+            fileItemFactory.setRepository(tmpDir);
         }
         // FileCleaningTracker fileCleaningTracker = FileCleanerCleanup.getFileCleaningTracker( servletContext ) ;
-        fileUpload = new ServletFileUpload( fileItemFactory ) ;
+        fileUpload = new ServletFileUpload(fileItemFactory);
         // upload.setFileSizeMax(MAX_FILE_SIZE);
         // upload.setSizeMax(MAX_REQUEST_SIZE);
     }
 
-    public Map<String, FileItem> parse( HttpServletRequest request ) throws FileUploadException {
+    public Map<String, FileItem> parse(HttpServletRequest request) throws FileUploadException {
         return fileUpload
-                .parseRequest( request )
+                .parseRequest(request)
                 .stream()
                 .collect(
                         Collectors.toMap(
                                 FileItem::getFieldName,
-                                Function.identity() ) ) ;
+                                Function.identity()));
     }
 }
