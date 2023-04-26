ae9d0a74c639aaa8b0e92ffadef277658652491b
pallyh <pallyx11@gmail.com>
2023-04-26 21:47:40 +0300

    Update CharsetFilter.java

--- WebBasics/src/main/java/itstep/learning/filter/CharsetFilter.java
+++ WebBasics/src/main/java/itstep/learning/filter/CharsetFilter.java
@@ -9,38 +9,38 @@ import java.io.IOException;
 
 @Singleton
 public class CharsetFilter implements Filter {
-    private FilterConfig filterConfig ;
+    private FilterConfig filterConfig;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        this.filterConfig = filterConfig ;
+        this.filterConfig = filterConfig;
     }
 
     @Override
     public void doFilter(
-            ServletRequest  servletRequest,   // !! обобщенный тип ServletRequest
-            ServletResponse servletResponse,  // реально это HttpServletRequest
-            FilterChain filterChain           // цепочка фильтров
-            ) throws IOException, ServletException
-    {
-        HttpServletRequest req = (HttpServletRequest) servletRequest ;
-        HttpServletResponse resp = (HttpServletResponse) servletResponse ;
-        req.setCharacterEncoding( "UTF-8" ) ;  // установка кодировки чтения из запроса. ДО ПЕРВОГО ЧТЕНИЯ
-        resp.setCharacterEncoding( "UTF-8" ) ;
+            ServletRequest servletRequest,   // !! обобщенный тип ServletRequest
+            ServletResponse servletResponse, // реально это HttpServletRequest
+            FilterChain filterChain          // цепочка фильтров
+    ) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        req.setCharacterEncoding("UTF-8"); // установка кодировки чтения из запроса. ДО ПЕРВОГО ЧТЕНИЯ
+        resp.setCharacterEncoding("UTF-8");
         // System.out.println("Forward");
         // цепочку фильтров необходимо продолжить. Иначе она будет прервана и запрос прекратит обработку
-        filterChain.doFilter( servletRequest, servletResponse ) ;
+        filterChain.doFilter(servletRequest, servletResponse);
         // после вызова цепочки - обратный ход (обработка ответа)
         // System.out.println("Backward");
     }
 
     @Override
     public void destroy() {
-        filterConfig = null ;
+        filterConfig = null;
     }
 }
 /*
 Фильтры (сервлетные фильтры) - сетевые классы для концепции Middleware -
-создания "цепочки" задач с возможностью внедрения кода как на этапе
+создание "цепочки" задач с возможностью внедрения кода как на этапе
 обработки запроса (прямой ход), так и на этапе обработки ответа (обратный ход)
 
 Задание: Создать фильтр formsFilter, встроить его после CharsetFilter
@@ -56,7 +56,6 @@ public class CharsetFilter implements Filter {
 Добавляем две зависимости
 <!-- https://mvnrepository.com/artifact/com.google.inject/guice -->
 <!-- https://mvnrepository.com/artifact/com.google.inject.extensions/guice-servlet -->
-
 - заменяем файл web.xml
 - создаем пакет ioc
   = класс ConfigListener extends GuiceServletContextListener
