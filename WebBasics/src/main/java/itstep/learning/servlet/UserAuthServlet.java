b2d12ee063a3e9907c6877726fd044bf11d67c02
pallyh <pallyx11@gmail.com>
2023-04-26 21:44:04 +0300

    Update UserAuthServlet.java

--- WebBasics/src/main/java/itstep/learning/servlet/UserAuthServlet.java
+++ WebBasics/src/main/java/itstep/learning/servlet/UserAuthServlet.java
@@ -2,7 +2,7 @@ package itstep.learning.servlet;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import itstep.learning.data.dao.UserDao;
+import itstep.learning.data.dao.IUserDao;
 import itstep.learning.data.entity.User;
 
 import javax.servlet.ServletException;
@@ -14,27 +14,32 @@ import java.io.IOException;
 @Singleton
 public class UserAuthServlet extends HttpServlet {
     @Inject
-    UserDao userDao ;
+    private IUserDao userDao;
+
+    @Inject
+    public UserAuthServlet(IUserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String authLogin = req.getParameter( "auth-login" ) ;
-        String authPass  = req.getParameter( "auth-pass"  ) ;
+        String authLogin = req.getParameter("auth-login");
+        String authPass = req.getParameter("auth-pass");
 
-        if( authPass != null && authLogin != null ) {
-            User user = userDao.getUserByCredentials( authLogin, authPass ) ;
-            if( user != null ) {
-                req.getSession().setAttribute( "authUser", user ) ;
-                resp.getWriter().print( "OK" ) ;
-                return ;
+        if (authPass != null && authLogin != null) {
+            User user = userDao.getUserByCredentials(authLogin, authPass);
+            if (user != null) {
+                req.getSession().setAttribute("authUser", user);
+                resp.getWriter().print("OK");
+                return;
             }
         }
-        resp.getWriter().print( "NO" ) ;
+        resp.getWriter().print("NO");
     }
 }
 /*
-    Авторизация - предоставление доступа ранее аутентифицированному пользовтелю
+    Авторизация - предоставление доступа ранее аутентифицированному пользователю
     Схемы:
      на сессиях (серверная) -- xSP (ASP, JSP, PHP) -- cookie передаются браузером
-     на токенах (клиентская) -- SPA -- не полагаемся на cookie, заменяя их своими токенами
+     на токенах (клиентская) -- SPA (React, Angular) -- не полагается на cookie, заменяя их своими токенами
  */
