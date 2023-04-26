28c6300ba3874b322fddc7244d86e9c0863d2bbb
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:07 +0300

    Update LoggerModule.java

--- WebBasics/src/main/java/itstep/learning/ioc/LoggerModule.java
+++ WebBasics/src/main/java/itstep/learning/ioc/LoggerModule.java
@@ -8,18 +8,15 @@ import java.util.logging.LogManager;
 public class LoggerModule extends AbstractModule {
     @Override
     protected void configure() {
-        try( InputStream conf =
-                 this.getClass()
-                     .getClassLoader()
-                     .getResourceAsStream( "logging.properties" ) )
+        try(InputStream conf =
+                    this.getClass()
+                            .getClassLoader()
+                            .getResourceAsStream("logging.properties"))
         {
-            LogManager logManager = LogManager.getLogManager() ;
-            logManager.reset() ;
-            logManager.readConfiguration( conf ) ;
+            LogManager.getLogManager().readConfiguration(conf);
         }
-        catch( Exception ex ) {
-            System.err.println( "LoggerModule::configure: " + ex.getMessage() ) ;
+        catch (Exception ex) {
+            System.err.println("LoggerModule::configure: " + ex.getMessage());
         }
     }
 }
-// LogManager.getLogManager().getProperty("java.util.logging.SimpleFormatter.format")
