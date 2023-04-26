7a8e697016a57a62fc9f7b37ed24495b4b918a77
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:13 +0300

    Update ServiceModule.java

--- WebBasics/src/main/java/itstep/learning/ioc/ServiceModule.java
+++ WebBasics/src/main/java/itstep/learning/ioc/ServiceModule.java
@@ -1,6 +1,8 @@
 package itstep.learning.ioc;
 
 import com.google.inject.AbstractModule;
+import itstep.learning.data.dao.IUserDao;
+import itstep.learning.data.dao.UserDao;
 import itstep.learning.service.DbService;
 import itstep.learning.service.HashService;
 import itstep.learning.service.LocalDbService;
@@ -11,8 +13,9 @@ import itstep.learning.service.auth.SessionAuthService;
 public class ServiceModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind( DbService.class   ).to( LocalDbService.class ) ;
-        bind( HashService.class ).to( Md5HashService.class ) ;
-        bind( AuthService.class ).to( SessionAuthService.class ) ;
+        bind(DbService.class).to(LocalDbService.class);
+        bind(HashService.class).to(Md5HashService.class);
+        bind(IUserDao.class).to(UserDao.class);
+        bind(AuthService.class).to(SessionAuthService.class);
     }
 }
