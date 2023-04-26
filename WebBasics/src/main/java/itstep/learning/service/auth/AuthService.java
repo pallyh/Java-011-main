8cb65c7ed65ffa87acb22879b54bcc8db2147efe
pallyh <pallyx11@gmail.com>
2023-04-26 21:39:41 +0300

    Update AuthService.java

--- WebBasics/src/main/java/itstep/learning/service/auth/AuthService.java
+++ WebBasics/src/main/java/itstep/learning/service/auth/AuthService.java
@@ -5,7 +5,7 @@ import itstep.learning.data.entity.User;
 import javax.servlet.http.HttpServletRequest;
 
 public interface AuthService {
-    void authorize( HttpServletRequest request ) ;
-    User getAuthUser() ;   // authUser | null
-    void logout( HttpServletRequest request ) ;
+    void authorize(HttpServletRequest request);
+    User getAuthUser(); // authUser | null
+    void logout(HttpServletRequest request);
 }
