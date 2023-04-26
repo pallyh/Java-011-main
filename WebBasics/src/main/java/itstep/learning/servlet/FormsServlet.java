0dfe6ff01d048714ad39c03b9b32921d9a46436c
pallyh <pallyx11@gmail.com>
2023-04-26 21:43:37 +0300

    Update FormsServlet.java

--- WebBasics/src/main/java/itstep/learning/servlet/FormsServlet.java
+++ WebBasics/src/main/java/itstep/learning/servlet/FormsServlet.java
@@ -19,87 +19,81 @@ import java.util.Date;
 @Singleton
 public class FormsServlet extends HttpServlet {
     @Inject
-    private HashService hashService ;
+    private HashService hashService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        FormsModel model = (FormsModel) session.getAttribute("model");
 
-        HttpSession session = req.getSession() ;
-        FormsModel model = (FormsModel) session.getAttribute( "model" ) ;
-        if( model == null ) {
-            model = parseModel( req ) ;
-        }
-        else {
-            session.removeAttribute( "model" ) ;
-        }
+        if (model == null) model = parseModel(req);
+        else session.removeAttribute("model");
 
-        req.setAttribute( "formsModel", model ) ;
-        req.getRequestDispatcher( "WEB-INF/forms.jsp" )
-                .forward( req, resp ) ;
+        req.setAttribute("formsModel", model);
+        req.getRequestDispatcher("WEB-INF/forms.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        FormsModel model = parseModel( req ) ;
-
-        HttpSession session = req.getSession() ;
-        session.setAttribute( "model", model ) ;
-        resp.sendRedirect( req.getRequestURI() ) ;
+        FormsModel model = parseModel(req);
+        HttpSession session = req.getSession();
+        session.setAttribute("model", model);
+        resp.sendRedirect(req.getRequestURI());
     }
 
-    private FormsModel parseModel(HttpServletRequest req ) {
-        FormsModel model = new FormsModel() ;
-        model.setMethod( req.getMethod() ) ;
-        // region "text"
-        String text = req.getParameter( "text" ) ;   // "text" - name of <input name="text" .../>
-        if( text == null ) {   // нет параметра (скорее всего нет передачи формы)
-            model.setText( "NULL" ) ;
-        }
-        else if( text.isEmpty() ) {   // параметр передан, но значение отсутствует
-            model.setText( "empty" ) ;
-        }
-        else {
-            model.setText( text + " " + hashService.getHexHash( text ) ) ;
+    private FormsModel parseModel(HttpServletRequest req) {
+        FormsModel model = new FormsModel();
+        model.setMethod(req.getMethod());
+        // region "string"
+        String stringParam = req.getParameter("string");
+        if (stringParam == null) { // нет параметра (скорее всего нет передачи формы)
+            model.setString("NULL");
+        } else if (stringParam.isEmpty()) { // параметр передан, но значение отсутствует
+            model.setString("empty");
+        } else {
+            model.setString(stringParam + " " + hashService.getHexHash(stringParam));
         }
         // endregion
         // region "number"
-        String numberParam = req.getParameter( "number" ) ;
-        if( numberParam == null ) {
-            model.setNumber( -1 ) ;
-        }
-        else if( numberParam.isEmpty() ) {
-            model.setNumber( 0 ) ;
-        }
-        else {
-            try { model.setNumber( Double.parseDouble( numberParam ) ) ; }
-            catch( NumberFormatException ignored ) { model.setNumber( -0.1 ) ; }
+        String numberParam = req.getParameter("number");
+        if (numberParam == null) {
+            model.setNumber(-1);
+        } else if (numberParam.isEmpty()) {
+            model.setNumber(0);
+        } else {
+            try {
+                model.setNumber(Double.parseDouble(numberParam));
+            } catch (NumberFormatException ignored) {
+                model.setNumber(-0.1);
+            }
         }
         // endregion
         // region "date"
-        String dateParam = req.getParameter( "date" ) ;
-        if( dateParam == null ) {
-            model.setDate( null ) ;
-        }
-        else if( dateParam.isEmpty() ) {
-            model.setDate( new Date() ) ;
-        }
-        else {
-            String dateFormat = "yyyy-MM-dd" ;
-            SimpleDateFormat format = new SimpleDateFormat( dateFormat ) ;
-            try { model.setDate( format.parse( dateParam ) ) ; }
-            catch( ParseException ignored ) { model.setDate( Date.from( Instant.EPOCH ) ) ; }
+        String dateParam = req.getParameter("date");
+        if (dateParam == null) {
+            model.setDate(null);
+        } else if (dateParam.isEmpty()) {
+            model.setDate(new Date());
+        } else {
+            String dateFormat = "yyyy-MM-dd";
+            SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+            try {
+                model.setDate(format.parse(dateParam));
+            } catch (ParseException ignored) {
+                model.setDate(Date.from(Instant.EPOCH));
+            }
         }
         // endregion
-        return model ;
+        // region "color"
+        String colorParam = req.getParameter("color");
+        if (colorParam == null) {
+            model.setColor(null);
+        } else if (colorParam.isEmpty()) {
+            model.setColor("#000000");
+        } else {
+            model.setColor(colorParam);
+        }
+        // endregion
+        return model;
     }
 }
-/*
-Д.З. Сверстать две формы на странице /forms
-Одна методом GET, другая POST, обе на action /forms
-Заложить передачу нескольких полей
-- строка
-- число
-- дата
-- цвет
- */
