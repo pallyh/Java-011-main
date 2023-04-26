8694405b7bdfd5013dd6913a9ccb9e79166c101e
pallyh <pallyx11@gmail.com>
2023-04-26 21:43:43 +0300

    Update StoryServlet.java

--- WebBasics/src/main/java/itstep/learning/servlet/StoryServlet.java
+++ WebBasics/src/main/java/itstep/learning/servlet/StoryServlet.java
@@ -1,6 +1,5 @@
 package itstep.learning.servlet;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -29,118 +28,106 @@ import java.util.UUID;
 
 @Singleton
 public class StoryServlet extends HttpServlet {
-    private final AuthService authService ;
-    private final DataContext dataContext ;
+    private final AuthService authService;
+    private final DataContext dataContext;
 
     @Inject
-    public StoryServlet( AuthService authService, DataContext dataContext ) {
-        this.authService = authService ;
-        this.dataContext = dataContext ;
+    public StoryServlet(AuthService authService, DataContext dataContext) {
+        this.authService = authService;
+        this.dataContext = dataContext;
     }
 
     @Override
-    protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
-        resp.setHeader( "Content-Type", "application/json" ) ;
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Content-Type", "application/json");
 
-        String taskId = req.getParameter( "task-id" ) ;
+        String taskId = req.getParameter("task-id");
         try {
-            UUID.fromString( taskId ) ;
+            UUID.fromString(taskId);
         }
-        catch( IllegalArgumentException ignored ) {
-            resp.setStatus( 400 ) ;
-            resp.getWriter().print( "\"Invalid parameter: task-id (UUID required)\"" ) ;
-            return ;
+        catch (IllegalArgumentException ignored) {
+            resp.setStatus(400);
+            resp.getWriter().print("\"Invalid parameter (UUID required)\"");
+            return;
         }
-        List<Story> stories = dataContext.getStoryDao().getListByTask( taskId ) ;
-        List<StoryViewModel> storyViewModels = new ArrayList<>() ;
-        for( Story story : stories ) {
+        List<Story> stories = dataContext.getStoryDao().getListByTask(taskId);
+        List<StoryViewModel> storyViewModels = new ArrayList<>();
+        for (Story story : stories) {
             StoryViewModel model = new StoryViewModel(
-                story,
-                dataContext.getUserDao().getById( story.getIdUser() ),
-                null
-            ) ;
-            if( story.getIdReply() != null ) {
-                Story reply = dataContext.getStoryDao().getById( story.getIdReply() ) ;
-                model.setReplyStory( new StoryViewModel(
-                    reply,
-                    dataContext.getUserDao().getById( reply.getIdUser() ),
+                    story,
+                    dataContext.getUserDao().getById(story.getIdUser()),
                     null
-                ) ) ;
+            );
+            if (story.getIdReply() != null) {
+                Story reply = dataContext.getStoryDao().getById(story.getIdReply());
+                model.setReplyStory(new StoryViewModel(reply, dataContext.getUserDao().getById(reply.getIdUser()), null));
             }
-            storyViewModels.add( model ) ;
+            storyViewModels.add(model);
         }
         resp.getWriter().print(
-            // new Gson().toJson( stories )
-            new GsonBuilder().serializeNulls().create().toJson( storyViewModels )
-        ) ;
+                 // new Gson().toJson(stories)
+                 new GsonBuilder().serializeNulls().create().toJson( storyViewModels )) ;
     }
 
     @Override
-    protected void doPost( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
-        String message = this.addStory( req ) ;
-        JSONObject result = new JSONObject() ;
-        result.put( "status", "OK".equals( message ) ? 200 : 400 ) ;
-        result.put( "message", message ) ;
-        resp.getWriter().print( result ) ;
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String message = this.addStory(req);
+        JSONObject result = new JSONObject();
+        result.put("status", "OK".equals(message) ? 200 : 400);
+        result.put("message", message);
+        resp.getWriter().print(result);
+    }
+
+    private String addStory(HttpServletRequest req) {
+        Story story = new Story();
+        User user = authService.getAuthUser();
+        if (user == null) {
+            return "Unauthorized";
+        }
+        story.setIdUser(user.getId());
+        String param = req.getParameter("story-text");
+        if (param == null || param.equals("")) {
+            return "Missing required parameter: story-text";
+        }
+        story.setContent(param);
+
+        param = req.getParameter("story-id-task");
+        if (param == null || param.equals("")) {
+            return "Missing required parameter: story-id-task";
+        }
+        try {
+            story.setIdTask(UUID.fromString(param));
+        } catch (RuntimeException ex) {
+            return "Invalid value: story-id-task";
+        }
+        return dataContext.getStoryDao().add(story) ? "OK" : "Inner error";
     }
 
     @Override
-    protected void doPut( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Test Email
         Properties prop = new Properties();
-        prop.put( "mail.smtp.auth",            "true" ) ;
-        prop.put( "mail.smtp.starttls.enable", "true" ) ;
-        prop.put( "mail.smtp.port",            "587"  ) ;
-        prop.put( "mail.smtp.ssl.trust",       "smtp.gmail.com" ) ;
-        prop.put( "mail.smtp.ssl.protocols",   "TLSv1.2" ) ;
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
-        Session mailSession = Session.getInstance( prop ) ;
-        // mailSession.setDebug( true ) ;  // log in console
-        Transport transport ;
+        Session mailSession = Session.getInstance( prop );
+        mailSession.setDebug( true ); // log in console
+        Transport transport;
         try {
-            MimeMessage message = new MimeMessage( mailSession ) ;
-            message.setFrom( new InternetAddress( "proviryalovich@gmail.com" ) ) ;
-            message.setSubject( "Hello from Java" ) ;
-            message.setContent( "PUT method works", "text/plain" ) ;
-
-            transport = mailSession.getTransport( "smtp" ) ;
-            transport.connect( "smtp.gmail.com", "proviryalovich@gmail.com", "umfqogmkoabhzpiu" ) ;
-            transport.sendMessage( message,
-                InternetAddress.parse("denniksam@gmail.com" ) ) ;
-            transport.close() ;
+            MimeMessage message = new MimeMessage(mailSession);
+            message.setFrom(new InternetAddress("project.recipe2023@gmail.com"));
+            message.setSubject("Hello from Java");
+            message.setContent("PUT method works", "text/plain");
+            transport = mailSession.getTransport("smtp");
+            transport.connect("smtp.gmail.com", "project.recipe2023@gmail.com", "pcwluhwwwfkitwtd");
+            transport.sendMessage(message, InternetAddress.parse("volodimirnazarisin@gmail.com"));
+            transport.close();
+        } catch (MessagingException ex) {
+            System.out.println(ex.getMessage());
         }
-        catch ( MessagingException ex ) {
-            System.out.println( ex.getMessage() ) ;
-        }
-
-        resp.getWriter().print( "PUT works" ) ;
-    }
-
-    private String addStory( HttpServletRequest req ) {
-        Story story = new Story() ;
-        User user = authService.getAuthUser() ;
-        if( user == null ) {
-            return  "Unauthorized" ;
-        }
-        story.setIdUser( user.getId() ) ;
-        String param = req.getParameter( "story-text" ) ;
-        if( param == null || param.equals( "" ) ) {
-            return "Missing required parameter: story-text" ;
-        }
-        story.setContent( param ) ;
-
-        param = req.getParameter( "story-id-task" ) ;
-        if( param == null || param.equals( "" ) ) {
-            return "Missing required parameter: story-id-task" ;
-        }
-        try {
-            story.setIdTask( UUID.fromString( param ) ) ;
-        }
-        catch( IllegalArgumentException ignored ) {
-            return "Invalid value: story-id-task" ;
-        }
-        return dataContext.getStoryDao().add( story )
-            ? "OK"
-            : "Inner error" ;
+        resp.getWriter().print("PUT works");
     }
 }
