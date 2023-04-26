101fd7a2ada1d1367f9f51e3d9c298fa6078e113
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:26 +0300

    Update AboutModel.java

--- WebBasics/src/main/java/itstep/learning/model/AboutModel.java
+++ WebBasics/src/main/java/itstep/learning/model/AboutModel.java
@@ -6,21 +6,18 @@ import java.util.Date;
  * View Model for about.jsp
  */
 public class AboutModel {
-    private String message ;
-    private Date moment ;
+    private String message;
+    private Date moment;
 
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
-
     public Date getMoment() {
         return moment;
     }
-
     public void setMoment(Date moment) {
         this.moment = moment;
     }
