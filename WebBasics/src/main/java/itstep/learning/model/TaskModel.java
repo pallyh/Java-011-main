7897554f1aaae9af4fc015770e8b666e05da0026
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:39 +0300

    Update TaskModel.java

--- WebBasics/src/main/java/itstep/learning/model/TaskModel.java
+++ WebBasics/src/main/java/itstep/learning/model/TaskModel.java
@@ -8,53 +8,42 @@ import java.util.Date;
 import java.util.UUID;
 
 public class TaskModel {
-    private String name ;    // name="task-name"
-    private UUID idTeam ;    // <select name="task-team">
-    private Date deadline ;  // name="task-deadline"
-    private byte priority ;  // <select name="task-priority">
-    private User author ;
+    private String name; // name="task-name"
+    private UUID idTeam; // <select name="task-team">
+    private Date deadline; // name="task-deadline"
+    private byte priority; // name="task-priority"
+    private User author;
+    private static final SimpleDateFormat formFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    private static final SimpleDateFormat formFormat =
-        new SimpleDateFormat( "yyyy-MM-dd" ) ;
-
-    // region Getters and Setters
+    // region accessors
     public String getName() {
         return name;
     }
-
-    public void setName( String name ) {
+    public void setName(String name) {
         this.name = name;
     }
-
     public UUID getIdTeam() {
         return idTeam;
     }
-
-    public void setIdTeam( String idTeam ) throws IllegalArgumentException {
-        this.idTeam = UUID.fromString( idTeam ) ;
+    public void setIdTeam(String idTeam) throws IllegalArgumentException {
+        this.idTeam = UUID.fromString(idTeam);
     }
-
     public Date getDeadline() {
         return deadline;
     }
-
-    public void setDeadline( String deadline ) throws ParseException {
-        this.deadline = formFormat.parse( deadline ) ;
+    public void setDeadline(String deadline) throws ParseException {
+        this.deadline = formFormat.parse(deadline);
     }
-
     public byte getPriority() {
         return priority;
     }
-
-    public void setPriority( byte priority ) {
+    public void setPriority(byte priority) {
         this.priority = priority;
     }
-
     public User getAuthor() {
         return author;
     }
-
-    public void setAuthor( User author ) {
+    public void setAuthor(User author) {
         this.author = author;
     }
     // endregion
