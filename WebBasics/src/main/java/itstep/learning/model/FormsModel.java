ea328047375ebd792229dd1b864ca50f655bdae1
pallyh <pallyx11@gmail.com>
2023-04-26 21:38:30 +0300

    Update FormsModel.java

--- WebBasics/src/main/java/itstep/learning/model/FormsModel.java
+++ WebBasics/src/main/java/itstep/learning/model/FormsModel.java
@@ -3,19 +3,21 @@ package itstep.learning.model;
 import java.util.Date;
 
 public class FormsModel {
-    private String text;
+    private String string;
     private double number;
     private Date date;
     private String color;
     private String method;
+
+
+    public String getString() {
+        return string;
+    }
+    public void setString(String message) {
+        this.string = message;
+    }
     public double getNumber() {
         return number;
-    }
-    public String getText() {
-        return text;
-    }
-    public void setText(String message) {
-        this.text = message;
     }
     public void setNumber(double number) {
         this.number = number;
@@ -26,19 +28,15 @@ public class FormsModel {
     public void setDate(Date date) {
         this.date = date;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public String getMethod() {
         return method;
     }
-
     public void setMethod(String method) {
         this.method = method;
     }
